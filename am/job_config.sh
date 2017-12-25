b32aba160a3f51867cce4b1d5eefbc610eed4c9a
Drew Titus <atitus@mit.edu>
2017-12-25 14:30:12 -0500

    Starting to debug AM baseline scripts

--- am/job_config.sh
+++ am/job_config.sh
@@ -1,9 +1,15 @@
 export EPOCH=0
-# export DATASET=/data/sls/scratch/haotang/ami/sls-data/ami-0.1
-export DATASET=$TEST_FEATS
+export DEBUG_MODEL=true
+if [ "$DEBUG_MODEL" = true ] ; then
+    export DATASET_NAME=debug
+    export DATASET=$TEST_FEATS
+else
+    export DATASET_NAME=ami-0.1
+    export DATASET=/data/sls/scratch/haotang/ami/sls-data/${DATASET_NAME}
+fi
 export DOMAIN=ihm
 
-export EXPT_NAME="${DATASET}/${DOMAIN}/frame-tdnn-450x7-step0.05"
+export EXPT_NAME="${DATASET_NAME}/${DOMAIN}/frame-tdnn-450x7-step0.05"
 
 export MODEL_DIR=$MODELS/am/$EXPT_NAME
 mkdir -p $MODEL_DIR
