3c5b2683dc4d42150569af5450f90bed1db0addd
Drew Titus <atitus@mit.edu>
2018-01-11 11:10:23 -0500

    More parameter combos; working on VAEs next?

--- cnn/job_config.sh
+++ cnn/job_config.sh
@@ -7,17 +7,17 @@ export LEARNING_RATE=0.00001
 export EPOCHS=100
 export BATCH_SIZE=256
 
-export ENC_CHANNELS=( 256 256 128 )
-export ENC_KERNELS=( 3 3 3 )        # Assume square kernels (AxA)
-export ENC_POOLS=( 3 3 0 )          # Pool only in frequency; no overlap. Use 0 to indicate no pooling
+export ENC_CHANNELS=( 256 256 128 64 )
+export ENC_KERNELS=( 3 3 3 3 )        # Assume square kernels (AxA)
+export ENC_POOLS=( 3 3 2 0 )          # Pool only in frequency; no overlap. Use 0 to indicate no pooling
 export ENC_FC=( 1024 1024 )     # Fully-connected layers following conv layers
 
 export LATENT_DIM=1024
 
 export DEC_FC=( 1024 1024 )     # Fully-connected layers before conv layers
-export DEC_CHANNELS=( 128 256 256 )
-export DEC_KERNELS=( 3 3 3 )        # Assume square kernels (AxA)
-export DEC_POOLS=( 0 3 3 )          # Pool only in frequency; no overlap. Use 0 to indicate no pooling
+export DEC_CHANNELS=( 64 128 256 256 )
+export DEC_KERNELS=( 3 3 3 3 )        # Assume square kernels (AxA)
+export DEC_POOLS=( 0 2 3 3 )          # Pool only in frequency; no overlap. Use 0 to indicate no pooling
 
 export ACTIVATION_FUNC=SELU
 
